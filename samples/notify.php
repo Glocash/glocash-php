ec4b9d95d99362c9829499daf378c9e71de55231
cs <2101653220@qq.com>
2021-05-13 14:14:44 +0800

    update php

--- samples/notify.php
+++ samples/notify.php
@@ -2,9 +2,8 @@
 /**
  * 异步通知
  * 技术联系人 陈荣江 17602115638 微信同号
- * 文档地址 https://portal.glocash.com/merchant/index/document
- * 商户后台 https://portal.glocash.com/merchant/index/login
- *
+ * 文档地址 https://docs.glocash.com/
+ * 商户后台 https://portal.glocashpayment.com/#/login
  */
 
 file_put_contents ('notify.log',var_export ($_POST,true).PHP_EOL,FILE_APPEND);
--- samples/pay.php
+++ samples/pay.php
@@ -4,7 +4,6 @@
  * 技术联系人 陈荣江 17602115638 微信同号
  * 文档地址 https://docs.glocash.com
  * 商户后台 https://portal.glocashpayment.com/#/login
- *
  */
 
 /**
